0d28324cca82aa49e31cbb4b5047daf39723a080
Mahir Minhaj K <kmahirminhaj@gmail.com>
2022-06-26 15:38:40 +0530

    added body

---
+++ TicTacGame.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = '0';
+
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char);
+
+int main()
+{
+    char winner = ' ';
+
+    resetBoard();
+
+    return 0;
+}
+
+void resetBoard()
+{
+}
+void printBoard()
+{
+}
+int checkFreeSpaces()
+{
+}
+void playerMove()
+{
+}
+void computerMove()
+{
+}
+char checkWinner()
+{
+}
+void printWinner(char winner)
+{
+}
